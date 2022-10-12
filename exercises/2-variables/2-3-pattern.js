4e9122d3e1851444f32d1528e63c91de863b75b1
Dikra Bouabdellah <dikra.bouabdellah@student.ehb.be>
2022-10-12 16:20:38 +0200

    oef pattern gestart

---
+++ exercises/2-variables/2-3-pattern.js
@@ -0,0 +1,14 @@
+"use strict";
+
+let canvas = document.querySelector('canvas');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+let context = canvas.getContext('2d');
+
+let width = context.canvas.width;
+let height = context.canvas.height;
+
+
+
+context.fillStyle = color;
+context.fillRect(x,y,size,size);
